b9f6a815f41b1171eeb5045f187e016995e83be6
ynazon <nazon@umich.edu>
2026-04-17 16:56:18 -0400

    Improves from the tablet

--- startScreen.py
+++ startScreen.py
@@ -3,21 +3,27 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.spinner import Spinner
+from kivy.uix.spinner import SpinnerOption
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.graphics import Color, RoundedRectangle
 
 
+class BigOption(SpinnerOption):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.font_size = 42
+
 # --- ROUNDED BUTTON ---
 class RoundedButton(Button):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
+        
         self.background_normal = ""  # removes default rectangle image
         self.background_color = (0, 0, 0, 0)  # fully transparent
 
         with self.canvas.before:
-            Color(0.2, 0.7, 1, 1)  # blue color
+            Color(0.965, 0.784, 0.208, 1)  # yellow color
             self.rect = RoundedRectangle(radius=[15], pos=self.pos, size=self.size)
 
         self.bind(pos=self.update_rect, size=self.update_rect)
@@ -29,70 +35,37 @@ class RoundedButton(Button):
 
 class MainLayout(BoxLayout):
     def __init__(self, **kwargs):
-        super().__init__(orientation='vertical', spacing=30, padding=200, **kwargs)
+        super().__init__(orientation='vertical', spacing=50, padding=200, **kwargs)
 
         # --- Image ---
-        self.add_widget(Image(source='figs/welcome.png', size_hint=(1, 0.5), pos_hint={"center_x": 0.5}))
+        self.add_widget(Image(source='figs/welcome.png', size_hint=(1, 0.75), pos_hint={"center_x": 0.5}))
 
         # --- Subject ID ---
-        subject_layout = BoxLayout(size_hint=(1, 0.1))
-        subject_layout.add_widget(Label(
-            text="Subject ID: \n(e.g. 001)",
-            bold=True,
-            font_size=42,
-            size_hint=(0.6, 1)
-        ))
+        subject_layout = BoxLayout(size_hint=(.6, 0.1),pos_hint={"center_x": 0.5})
+        subject_layout.add_widget(Label(text="Subject ID: \n(e.g. 001)", bold=True, font_size=60, size_hint=(0.4, 1)))
 
-        self.subject_input = TextInput(multiline=False, font_size=34, font_name="Roboto-Bold.ttf")
+        self.subject_input = TextInput(multiline=False, font_size=80, font_name="Roboto-Bold.ttf", size_hint=(0.6, 1))
         subject_layout.add_widget(self.subject_input)
         self.add_widget(subject_layout)
 
         # --- Trial Condition ---
-        trial_type_layout = BoxLayout(size_hint=(1, 0.1))
-        trial_type_layout.add_widget(Label(
-            text="Trial Condition:",
-            bold=True,
-            font_size=42,
-            size_hint=(0.6, 1)
-        ))
+        trial_type_layout = BoxLayout(size_hint=(0.6, 0.1),pos_hint={"center_x": 0.5})
+        trial_type_layout.add_widget(Label(text="Trial Condition:", bold=True,font_size=60, size_hint=(0.4, 1)))
 
-        self.trial_type_spinner = Spinner(
-            text="Select Condition",
-            font_size=36,
-            bold = True,
-            values=["TH ~ Take Home", "PF ~ Proflex", "VS ~ VSPA"]
-        )
+        self.trial_type_spinner = Spinner(text="Select Condition", font_size=58, bold = True,values=["TH ~ Take Home", "PF ~ Proflex", "VS ~ VSPA"], size_hint=(0.6, 1),option_cls=BigOption)
         trial_type_layout.add_widget(self.trial_type_spinner)
         self.add_widget(trial_type_layout)
 
         # --- Trial Number ---
-        trial_num_layout = BoxLayout(size_hint=(1, 0.1))
-        trial_num_layout.add_widget(Label(
-            text="Trial Number:",
-            font_size=42,
-            bold = True,
-            size_hint=(0.6, 1)
-        ))
+        trial_num_layout = BoxLayout(size_hint=(0.6, 0.1), pos_hint={"center_x": 0.5})
+        trial_num_layout.add_widget(Label(text="Trial Number:", font_size=60, bold = True, size_hint=(0.4, 1)))
 
-        self.trial_num_spinner = Spinner(
-            text="Select Trial",
-            bold = True,
-            font_size=36,
-            values=["1", "2"]
-        )
+        self.trial_num_spinner = Spinner(text="Select Trial", size_hint=(0.6, 1), bold = True, font_size=58, values=["1", "2"], option_cls=BigOption)
         trial_num_layout.add_widget(self.trial_num_spinner)
         self.add_widget(trial_num_layout)
 
         # --- BEGIN BUTTON (ROUNDED) ---
-        self.begin_btn = RoundedButton(
-            text="Begin",
-            size_hint=(.25, 0.13),
-            pos_hint={"center_x":0.5},
-            disabled=True,
-            font_size=34,
-            bold = True,
-            color=(1, 1, 1, 1)  # text color white
-        )
+        self.begin_btn = RoundedButton(text="START",size_hint=(.15, 0.15), pos_hint={"center_x":0.5},disabled=True,font_size=80,bold = True,color=(0, 0, 0, 1))  # text color white
 
         self.begin_btn.bind(on_press=self.start_trial)
         self.add_widget(self.begin_btn)
